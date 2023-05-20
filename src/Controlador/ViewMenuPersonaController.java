e79b7a6e9c08ab7ee144963569aa7c6def3eddcc
AlejandroSJ9 <alejandro.salas090603@gmail.com>
2023-05-20 10:23:48 -0500

    Nueva clase para el controller del menu usuario

---
+++ src/Controlador/ViewMenuPersonaController.java
@@ -0,0 +1,17 @@
+package Controlador;
+
+import Modelo.PersonaModel;
+
+public class ViewMenuPersonaController {
+    private PersonaModel personaModel;
+    private DataBase dataBase;
+
+    public ViewMenuPersonaController(PersonaModel personaModel, DataBase dataBase) {
+        this.personaModel = personaModel;
+        this.dataBase = dataBase;
+    }
+
+    public void iniciarVista(){
+
+    }
+}
