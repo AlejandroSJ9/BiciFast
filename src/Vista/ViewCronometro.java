0e0f71e73ebf889c99309f7c252bd347210ad870
FabianReina <FabianReina20@hotmail.com>
2023-05-22 01:05:51 -0500

    Se agrego visual de Cronometro

---
+++ src/Vista/ViewCronometro.java
@@ -0,0 +1,53 @@
+package Vista;
+
+import Modelo.CronometroModel;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ViewCronometro {
+    private JFrame frame;
+    private JButton startButton;
+    private JButton pauseButton;
+    private CronometroModel cronometro;
+
+    public ViewCronometro(){
+        frame= new JFrame("Cronometro");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JPanel panel=new JPanel();
+
+        startButton= new JButton("iniciar");
+        startButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                iniciarCronometro();
+            }
+        });
+        panel.add(startButton);
+
+        pauseButton= new JButton("Pausar");
+        pauseButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                pausarCronometro();
+            }
+        });
+
+        panel.add(pauseButton);
+        frame.add(panel);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    private void iniciarCronometro(){
+        cronometro= new CronometroModel();
+        cronometro.iniciar();
+    }
+
+    private void pausarCronometro(){
+        cronometro= new CronometroModel();
+        cronometro.pausar();
+    }
+}
